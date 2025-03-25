5fba38992ee82bdf66a713d7d4346d114c9725a6
Kevin Wilkins <kwilkinsrd@gmail.com>
2025-03-25 09:55:54 -0500

    fix: Convert to splat
    Updated script to use splats instead of backticks.
    This will improve readability and scalability.

--- user_management/ResetPassword.ps1
+++ user_management/ResetPassword.ps1
@@ -9,19 +9,19 @@ sign in.
 $config = Import-PowershellDataFile -Path .\mstenant.psd1
 $tenant = $config.domain
 
-$userName = Read-Host "Username: "
+$userName = Read-Host "Username"
 $passwd = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 12 | ForEach-Object {[char]$_})
 
-Update-MgUser `
-    -UserId "$userName@$tenant" `
-    -PasswordProfile @{
-        ForceChangePasswordNextSignIn = $True;
-        Password = "$passwd"
-    } `
-
-Write-Host @"
-Password changed for $userName.
-
-New password: $password
-
-"@
+$passwordProfile = @{
+    ForceChangePasswordNextSignIn = $True
+    Password = "$passwd"
+}
+$updateMgUser = @{
+    UserId = "$username@$tenant"
+    PasswordProfile = $passwordProfile
+}
+$output = @{
+    Content = "`nPassword changed for $userName`nPassword: $passwd`n"
+}
+Update-MgUser @updateMgUser
+Write-Host $output.Content
